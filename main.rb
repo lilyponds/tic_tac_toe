4dec92ada01d3bc8445d8e245e3dc4cec22df3be
tg <157854871+lilyponds@users.noreply.github.com>
2024-07-08 17:45:20 -0500

    Add vertical and horizontal traversing to find 3 in a row. Pending diagonal traversing.

--- main.rb
+++ main.rb
@@ -1,11 +1,14 @@
 class NewGrid
     @@turn = 0
+    @@transpose = Array.new(3) {Array.new(3,"_")}
     def initialize
         @grid = Array.new(3) {Array.new(3,"_")}
     end
     def rough_grid
         @grid
     end
+    
+
     def grid
         self.rough_grid.each { |line| puts line.join (" | ") }
     end
@@ -13,10 +16,11 @@ class NewGrid
     def status
         self.grid
         if @@turn > 4
-            if self.rough_grid.any? { |line| line.all?("X") || line.all?(0)}
+            if self.rough_grid.any? { |line| line.all?("X") || line.all?(0)} || @@transpose.any? { |line| line.all?("X") || line.all?(0)}
             puts "Winner Winner!!"
             @@turn = 0
             @grid = Array.new(3) {Array.new(3,"_")}
+            @@transpose = Array.new(3) {Array.new(3,"_")}
         else
             end
         else
@@ -27,8 +31,10 @@ class NewGrid
         if self.rough_grid[xpos][ypos] == "_"
             if @@turn.even?
                 self.rough_grid[xpos][ypos] = "X"
+                @@transpose[ypos][xpos] = "X"
             else
                 self.rough_grid[xpos][ypos] = "0"
+                @@transpose[ypos][xpos] = "0"
             end
             @@turn +=1
             self.status
